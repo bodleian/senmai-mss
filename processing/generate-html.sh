eb0f74169c938d99ee5ec92320d968bc6605f7d0
Andrew Morrison <andrew.morrison@bodleian.ox.ac.uk>
2018-04-16 13:52:38 +0100

    Production indexing script

--- processing/generate-html.sh
+++ processing/generate-html.sh
@@ -16,4 +16,4 @@ LOGFILE="html/html.log"
 echo "Transforming TEI files in collections folder using convert2HTML.xsl on $(date +"%Y-%m-%d %H:%M:%S") to create HTML manuscript view pages." > $LOGFILE
 
 # Run XSLT on all TEI files in collections path (using pwd to get full path, not relative, which is what the XSL needs)
-java -Xmx1G -Xms1G -cp "saxon/saxon9he.jar"  net.sf.saxon.Transform -it:batch -xsl:convert2HTML.xsl collections-path=`pwd`/../collections/ 2>> $LOGFILE
+java -Xmx1G -cp "saxon/saxon9he.jar"  net.sf.saxon.Transform -it:batch -xsl:convert2HTML.xsl collections-path=`pwd`/../collections/ 2>> $LOGFILE
---
+++ processing/index-all-prd.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Command arguments
+# $1 = Optional mode:
+#           Append 'force' to disable checking for data issues and push to Solr without prompting
+#           Append 'noindex' to generate the files and do the checking but not push to Solr
+
+if [ ! "$1" == "force" ]; then
+    # Give up if any one index fails or is abandoned
+    set -e
+fi
+
+# Re-index manuscripts (includes rebuilding customized manuscript HTML pages, which must be run first)
+./generate-html.sh && ./generate-solr-document.sh manuscripts.xquery manuscripts_index.xml manuscript solr01-prd.bodleian.ox.ac.uk $1
+
+# Reindex works
+./generate-solr-document.sh works.xquery works_index.xml work solr01-prd.bodleian.ox.ac.uk $1
+
+# Reindex people
+./generate-solr-document.sh persons.xquery persons_index.xml person solr01-prd.bodleian.ox.ac.uk $1
+
+# Senmai TEI files do not currently support a fourth index
